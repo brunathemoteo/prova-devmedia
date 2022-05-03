1713e12c4ca26751bc2c5c743bcfdd028e11deb5
brunathemoteo <brunathemoteo@gmail.com>
2022-05-02 22:37:15 -0300

    ajustes no css

--- index.js
+++ index.js
@@ -1,82 +1,85 @@
-const express = require('express');
-const { engine }  = require('express-handlebars')
+const express = require('express')
+const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
-const Noticia = require('./models/Noticia');
+const Noticia = require('./models/Noticia')
 const Op = Sequelize.Op
 
-const app = express();
+const app = express()
 app.use('/public', express.static(__dirname + '/public'))
 
 // Config
-    // Template Engine
-        app.engine('handlebars', engine())
-        app.set('view engine', 'handlebars')
-        app.set('views', './views')
+// Template Engine
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
-    // Body Parser (pega dados do formulario)
-        app.use(bodyParser.urlencoded({extended: false}))
-        app.use(bodyParser.json())
+// Body Parser (pega dados do formulario)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Rotas
-
-    app.get('/home', (req, res) => {
-        Noticia.findAll({order: [['id', 'ASC']]}).then(function(posts){
-            res.render('home', {posts: posts})
-        })
+app.get('/home', (req, res) => {
+    Noticia.findAll({ order: [['id', 'ASC']] }).then(function (posts) {
+        res.render('home', { posts: posts })
     })
+})
 
-    app.get('/cadastro', (req, res) => {
-        res.render('formulario')
+app.get('/cadastro', (req, res) => {
+    res.render('formulario')
+})
+
+app.post('/add', function (req, res) {
+    Noticia.create({
+        tituloNoticia: req.body.tituloNoticia,
+        categoriaNoticia: req.body.categoriaNoticia,
+        conteudoNoticia: req.body.conteudoNoticia
     })
-
-    app.post('/add', function(req, res){
-        Noticia.create({
-            tituloNoticia: req.body.tituloNoticia,
-            categoriaNoticia: req.body.categoriaNoticia,
-            conteudoNoticia: req.body.conteudoNoticia
-        }).then(function(){
+        .then(function () {
             res.redirect('/')
-        }).catch(function(erro){
+        })
+        .catch(function (erro) {
             res.send('Houve um erro: ' + erro)
         })
-    })
-    
-    app.post('/consulta', function(req, res){
-        Noticia.findAll({
-            where: { 
-                [Op.or]: [{ 
+})
+
+app.post('/consulta', function (req, res) {
+    Noticia.findAll({
+        where: {
+            [Op.or]: [
+                {
                     tituloNoticia: {
-                        [Op.like]: `%`+req.body.idconsulta+`%`
+                        [Op.like]: `%` + req.body.idconsulta + `%`
                     }
                 },
                 {
-                    categoriaNoticia: 
-                        {[Op.like]: `%`+req.body.idconsulta+`%`
+                    categoriaNoticia: {
+                        [Op.like]: `%` + req.body.idconsulta + `%`
                     }
-                }    
+                }
             ]
-        }  
-    }).then(function(posts){
-            res.render('home', {posts: posts})
-        })
+        }
+    }).then(function (posts) {
+        res.render('home', { posts: posts })
     })
+})
 
-    app.get('/acessar/:id', function(req, res){
-        Noticia.findAll({ where: {'id' : req.params.id}}).then(function(noticias)
-        {
-            res.render('noticia',{noticias: noticias})
-        })
+app.get('/acessar/:id', function (req, res) {
+    Noticia.findAll({ where: { id: req.params.id } }).then(function (noticias) {
+        res.render('noticia', { noticias: noticias })
     })
+})
 
-    app.get('/deletar/:id', function(req, res){
-        Noticia.destroy({where: {'id' : req.params.id}}).then(function(){
+app.get('/deletar/:id', function (req, res) {
+    Noticia.destroy({ where: { id: req.params.id } })
+        .then(function () {
             res.send('Noticia deletada com sucesso')
-        }).catch(function(erro){
+        })
+        .catch(function (erro) {
             res.send('Esta notícia nao existe!')
         })
-    })
+})
 
-app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081");
-});
+app.listen(8081, function () {
+    console.log('Servidor rodando na url http://localhost:8081')
+})
